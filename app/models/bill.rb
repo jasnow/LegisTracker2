8c9fca7c5a12086dce4f6e6af960a4c041f73236
Al Snow <sqctest02@gmail.com>
2013-02-07 20:17:12 -0500

    moved :id in app/models/bill.rb

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,9 +2,9 @@ require 'bill_status_summary'
 require 'shellwords'
 
 class Bill < ActiveRecord::Base
-  attr_accessible :id, :btype, :num, :number, :short_title
+  attr_accessible :btype, :num, :number, :short_title
   attr_accessible :title, :b_status, :status_code_id, :crossover
-  attr_accessible :user_id, :user, :bill_id
+  attr_accessible :id, :user_id, :user, :bill_id
 
   acts_as_taggable_on :topics, :hot
   

e45922c699b1530ec6d70197f121d97cdd9184a2
Al Snow <sqctest02@gmail.com>
2013-02-09 15:04:59 -0500

    Fix [undefined method 'request_uri'] error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,13 @@
 module ApplicationHelper
 
   def current_class?(test_path)
-    return 'current' if request.request_uri == test_path
+    return 'current' if request.fullpath == test_path
     ''
   end
 
   def current1_class?(test_path)
-    return 'current1' if request.request_uri == test_path
+    return 'current1' if request.full_path == test_path
     ''
   end
-
-
 end
+

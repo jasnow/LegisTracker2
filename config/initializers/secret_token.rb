f5a6a7b11e8f0aa4b99fcd98714bf273059384f1
Al Snow <sqctest02@gmail.com>
2013-02-09 17:13:42 -0500

    Fix secret_token in github issue

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-LegisTracker2::Application.config.secret_token = 'afbf7fbe142a5b27795a53f5c84801ea7cec890b2d1df8832bc6fb911c1c33ca102f72dc26400a123373ebbdb9f4114d90d5cb8d6b40cd990fcc31472ebcac12'
+LegisTracker2::Application.config.secret_token = ENV["SEC_TOKEN"]

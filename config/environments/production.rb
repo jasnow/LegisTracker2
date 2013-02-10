dccae8f264a43c1c18333041afc2a911b5a56090
Al Snow <sqctest02@gmail.com>
2013-02-10 11:09:33 -0500

    fixed x31x vendor/plugins warnings msgs on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ LegisTracker2::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
+  config.active_support.deprecation = :silence
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)

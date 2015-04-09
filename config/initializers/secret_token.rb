1f7b70d0315edbafd995fad8762b5ba59686c6f4
Al Snow <jasnow@hotmail.com>
2015-04-09 14:23:37 -0400

    Fixed sec env name; fix jquery-ui.js dir

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-LegisTracker2::Application.config.secret_token = ENV["SEC_TOKEN"]
+LegisTracker2::Application.config.secret_token = ENV["SEC_TOKEN_SP32"]

85c908a7ded8093978550b19c32e2f478d924c64
Al Snow <jasnow@hotmail.com>
2019-07-06 07:35:56 -0400

    Changed factory_girl to factory_bot

--- config/application.rb
+++ config/application.rb
@@ -77,7 +77,7 @@ module LegisTracker2
     # 2/7/2013: Apped for RailsApp.
     config.generators do |g|
       g.test_framework :rspec, fixture: true
-      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
       g.view_specs false
       g.helper_specs false
       g.stylesheets = false
